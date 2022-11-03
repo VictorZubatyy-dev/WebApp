9619a49ae5b1545a540ce05266b722677eb48bb8
VictorZubatyy-dev <victorzubatyy1@gmail.com>
2022-11-03 11:59:31 +0000

    Added database connection

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+await mongoose.connect('mongodb://localhost/my_database');
+
+
+const connectDB = async () => {
+    try{
+        const conn = await mon
+    }
+}
